d2d54e24d44a8af76db7b60d0efd2ac12e994635
Blaine Stockman <blaine-stockman@uiowa.edu>
2025-11-21 13:08:03 -0600

    Updated app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["SECRET_KEY"] = "by837y4r9gbf39fg87g39f4"
 CORS(app)
 
 # Get this URL from the Azure Overview page of your API web app
-api_url = "bstockman-mod12-api.azurewebsites.net"  # base url for API endpoints
+api_url = "http://127.0.0.1:5002"  # base url for API endpoints
 
 
 # main index page route
